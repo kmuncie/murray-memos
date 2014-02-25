b5e75941e1a549dce1032419b9abda1c81a0edb6
KMuncie <nivek2002@gmail.com>
2014-02-25 14:41:47 -0500

    Lint free!

--- app/js/app.js
+++ app/js/app.js
@@ -25,56 +25,51 @@ var app = (function() {
 	app.publicFunction();
 	app.userAgentInit();
 
-	$("textarea").height( $("textarea")[0].scrollHeight+20 );
+	$('textarea').height( $('textarea')[0].scrollHeight+20 );
 
 })();
 
-var noteTaker = (function() {
+(function() {
 	'use strict';
 
 	//  For generating GUID
 	function s4() {
-		return Math.floor((1 + Math.random()) * 0x10000)
-		          .toString(16)
-		          .substring(1);
-	};
+		return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+	}
 
 	function guid() {
-		return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-		      s4() + '-' + s4() + s4() + s4();
-	};
+		return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+	}
 
 	// Human readable timestamp
 	function formatTimeOfDay(millisSinceEpoch) {
-	  var secondsSinceEpoch = (millisSinceEpoch / 1000) | 0;
-	  var secondsInDay = ((secondsSinceEpoch % 86400) + 86400) % 86400;
-	  var seconds = secondsInDay % 60;
-	  var minutes = ((secondsInDay / 60) | 0) % 60;
-	  var hours = (secondsInDay / 3600) | 0;
-	  return hours + (minutes < 10 ? ":0" : ":")
-	      + minutes + (seconds < 10 ? ":0" : ":")
-	      + seconds;
+		var secondsSinceEpoch = (millisSinceEpoch / 1000) || 0;
+		var secondsInDay = ((secondsSinceEpoch % 86400) + 86400) % 86400;
+		var seconds = secondsInDay % 60;
+		var minutes = ((secondsInDay / 60) || 0) % 60;
+		var hours = (secondsInDay / 3600) || 0;
+		return hours + (minutes < 10 ? ':0' : ':') + minutes + (seconds < 10 ? ':0' : ':') + seconds;
 	}
 
 	// The note object
-	function note() {
-		this.id = guid(),
-		this.meta = formatTimeOfDay($.now()),
-		this.title = 'A note of epic proportion',
-		this.body = 'Please type a note here...'
-	};
+	function Note() {
+		this.id = guid();
+		this.meta = formatTimeOfDay($.now());
+		this.title = 'A note of epic proportion';
+		this.body = 'Please type a note here...';
+	}
 
-	var singleNote = new note();
+	var singleNote = new Note();
 
 	// Persistor for saving a getting from localStorage
 	var persistor = {
 
 		save: function(note) {
-			localStorage['aNote'] = JSON.stringify(note);
+			localStorage.aNote = JSON.stringify(note);
 		},
 
 		get: function() {
-			var aNoteLS = JSON.parse(localStorage['aNote']);
+			var aNoteLS = JSON.parse(localStorage.aNote);
 			return aNoteLS;
 		}
 	};
@@ -92,19 +87,19 @@ var noteTaker = (function() {
 		console.log(singleNote);
 
 		persistor.save(singleNote);
-	};
+	}
 
 	// Load the only note in localStorage
 	function loadAction(note) {
 		$('.meta').text(note.meta);
 		$('#noteTitle').val(note.title);
 		$('#noteBody').val(note.body);
-	};
+	}
 
 	// On page load retrieve and display the note in localStorage
 	$( document ).ready(function() {
-  		var myNote = persistor.get();
-  		loadAction(myNote);
+		var myNote = persistor.get();
+		loadAction(myNote);
 	});
 
 	$('#save').click(saveAction);
